ac1174fa06a86b27c170e5a76a2fc06ac7497f9e
Lucas Santos <lhs.santoss@gmail.com>
2021-04-26 16:02:24 -0300

    Add proto build script

---
+++ scripts/build-protofiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pwd | grep 'scripts' || cd scripts || return
+
+protoc \
+  --proto_path=../protos \
+  --js_out=import_style=commonjs:../protos \
+  ../protos/github.com/containerd/containerd/api/services/namespaces/v1/*.proto \
+  ../protos/github.com/containerd/containerd/api/services/containers/v1/*.proto \
+  ../protos/github.com/containerd/containerd/api/services/images/v1/*.proto \
+  ../protos/github.com/containerd/containerd/api/types/*.proto \
+  ../protos/gogoproto/gogo.proto
