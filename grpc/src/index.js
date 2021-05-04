592c888f67339e974989c77cc847e1e72fc1ef72
Lucas Santos <lhs.santoss@gmail.com>
2021-05-04 17:14:23 -0300

    move things to grpc dir

--- grpc/src/index.js
+++ grpc/src/index.js
@@ -9,14 +9,12 @@ const path = require('path');
     ],
     keepCase: true
   }
-
   const imageDefinition = await protoLoader.load(path.resolve(__dirname, '../protos/github.com/containerd/containerd/api/services/images/v1/images.proto'), options)
   const imageObject = grpc.loadPackageDefinition(imageDefinition)
-  const imageClient = new imageObject.containerd.services.images.v1.Images(`unix:${process.env.CONTAINERD_SOCKET_PATH || '/run/containerd/containerd.sock'}`, grpc.credentials.createInsecure())
+  const imageClient = new imageObject.containerd.services.images.v1.Images('unix:/run/containerd/containerd.sock', grpc.credentials.createInsecure())
 
   const metadata = new grpc.Metadata()
-  metadata.add('containerd-namespace', process.env.CONTAINERD_NAMESPACE || 'jsctr')
-
+  metadata.add('containerd-namespace', 'jsctr')
   imageClient.List({ filters: [] }, metadata, (err, images) => {
     if (err) throw err
     console.log(images)
