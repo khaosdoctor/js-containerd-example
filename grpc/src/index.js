432f5fefe38627060af0b410fd30432ff13b298e
Lucas Santos <lhs.santoss@gmail.com>
2021-05-05 22:28:19 -0300

    Add content list

--- grpc/src/index.js
+++ grpc/src/index.js
@@ -1,6 +1,7 @@
 const protoLoader = require('@grpc/proto-loader')
 const grpc = require('@grpc/grpc-js')
-const path = require('path');
+const path = require('path')
+const fs = require('fs');
 
 (async () => {
   const options = {
@@ -13,10 +14,40 @@ const path = require('path');
   const imageObject = grpc.loadPackageDefinition(imageDefinition)
   const imageClient = new imageObject.containerd.services.images.v1.Images('unix:/run/containerd/containerd.sock', grpc.credentials.createInsecure())
 
-  const metadata = new grpc.Metadata()
-  metadata.add('containerd-namespace', 'jsctr')
-  imageClient.List({ filters: [] }, metadata, (err, images) => {
+  const namespace = new grpc.Metadata()
+  namespace.add('containerd-namespace', 'jsctr')
+
+  const payload = {
+    image: {
+      name: 'docker.io/khaosdoctor/go-vote-api:latest',
+      target: {
+        size: 2201697,
+        digest: 'sha256:51dd0ec4b871707363699fcc75b7d7538cfa8f53cc83c8c82ef57acbc9a57fc9',
+        media_type: 'application/vnd.docker.distribution.manifest.v2+json'
+      },
+      created_at: new Date,
+      updated_at: new Date
+    }
+  }
+
+  imageClient.Create(payload, namespace, (err, result) => {
+    if (err) throw err
+    console.log(result)
+  })
+
+  imageClient.List({ filters: [] }, namespace, (err, images) => {
     if (err) throw err
     console.log(images)
+  })
+
+  const contentDefinition = await protoLoader.load(path.resolve(__dirname, '../protos/github.com/containerd/containerd/api/services/content/v1/content.proto'), options)
+  const contentObject = grpc.loadPackageDefinition(contentDefinition)
+  const contentClient = new contentObject.containerd.services.content.v1.Content('unix:/run/containerd/containerd.sock', grpc.credentials.createInsecure())
+
+  const contentStream = contentClient.List({ filters: [] }, namespace)
+  if (!fs.statSync(path.resolve(__dirname, './outputs'))) fs.mkdirSync(path.resolve(__dirname, './outputs'))
+  contentStream.on('data', (content, err) => {
+    if (err) throw err
+    fs.writeFileSync(path.resolve(__dirname, './outputs/content.json'), JSON.stringify(content, null, 2))
   })
 })()
